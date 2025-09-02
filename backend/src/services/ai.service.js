53352c8501d966a9a27c57aced162574ab822f56
itxzmevishal <sanapvishal128@gmail.com>
2025-09-03 01:13:40 +0530

    Initial commit

---
+++ backend/src/services/ai.service.js
@@ -0,0 +1,14 @@
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GEMINI_KEY);
+const model = genAI.getGenerativeModel({
+  model: "gemini-2.5-pro",
+  systemInstruction:
+    "You are an elite AI Code Reviewer with expertise equivalent to a Principal Software Engineer and Technical Architect with 15+ years of hands-on experience in multiple programming languages, frameworks, and system design. You deeply understand clean coding principles, performance optimization, security, scalability, maintainability, and debugging best practices. Your mission is to analyze any provided code — regardless of language, complexity, or context — and produce clear, structured, and highly actionable feedback that accelerates developer productivity and code quality. Highlight Errors With Red Cross ❌ & Solutions With Green Check ✅.",
+});
+
+async function generateContent(prompt) {
+  const result = await model.generateContent(prompt);
+  return result.response.text();
+}
+
+module.exports = generateContent;
---
+++ frontend/src/App.jsx
@@ -0,0 +1,61 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import "prismjs/themes/prism-tomorrow.css";
+import prism from "prismjs";
+import Editor from "react-simple-code-editor";
+import axios from "axios";
+import Markdown from "react-markdown";
+import rehypeHighlight from "rehype-highlight";
+import 'highlight.js/styles/github-dark.css';
+
+
+
+
+function App() {
+  const [code, setCode] = useState(``);
+const [review, setReview] = useState(``)
+  useEffect(() => {
+    prism.highlightAll();
+  }, []);
+
+  async function reviewCode() {
+  const response = await  axios.post('http://localhost:3000/ai/get-review', { code } )
+  setReview(response.data)
+  
+  }
+  return (
+    <>
+      <main>
+        <div className="left">
+          <div className="code">
+            <Editor
+              value={code}
+              placeholder="Enter your code here"
+              onValueChange={setCode}
+              highlight={(code) =>
+                prism.highlight(code, prism.languages.javascript)
+              }
+              padding={10}
+              style={{
+                overflow: "scroll",
+                fontSize: 17,
+                border: "1px solid #ddd",
+                borderRadius: "5px",
+                height: "100%",
+                width: "100%",
+              }}
+            />
+          </div>
+          <div onClick={reviewCode} className="review">
+            Review
+          </div>
+        </div>
+        <div className="right"><Markdown 
+        rehypePlugins={[rehypeHighlight]}
+        >{review}</Markdown></div>
+      </main>
+    </>
+  );
+}
+
+export default App;
