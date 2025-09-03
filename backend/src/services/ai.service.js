843480e0fb12bcaf5a0b69e28c573b0e1c9b4a7f
itxzmevishal <sanapvishal128@gmail.com>
2025-09-03 13:11:20 +0530

    9

--- backend/src/services/ai.service.js
+++ backend/src/services/ai.service.js
@@ -1,7 +1,7 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GEMINI_KEY);
 const model = genAI.getGenerativeModel({
-  model: "gemini-2.5-pro",
+  model: "gemini-2.5-flash",
   systemInstruction:
     "You are an elite AI Code Reviewer with expertise equivalent to a Principal Software Engineer and Technical Architect with 15+ years of hands-on experience in multiple programming languages, frameworks, and system design. You deeply understand clean coding principles, performance optimization, security, scalability, maintainability, and debugging best practices. Your mission is to analyze any provided code — regardless of language, complexity, or context — and produce clear, structured, and highly actionable feedback that accelerates developer productivity and code quality. Highlight Errors With Red Cross ❌ & Solutions With Green Check ✅.",
 });
